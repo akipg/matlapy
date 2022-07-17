19ed39a5a63e0316bc2145c324b9067f79a24a57
sa-ji <108157287+sa-ji@users.noreply.github.com>
2022-07-17 23:34:36 +0900

    add slx.py

--- test3.py
+++ test3.py
@@ -7,4 +7,14 @@ mat.connect_first_matlab()
 
 mat.eng.cd(str(Path().absolute()))
 
+# mat.eng.close_system("testsys", 0, "CloseReferencedModels", "on",  nargout=0)
+
+# mat.eng.new_system("testsys", nargout=0)
+
+# mat.eng.open_system("testsys", nargout=0)
+
+model = mat.Slx("test1")
+
+
+
 #mat.eng.load_system("")
